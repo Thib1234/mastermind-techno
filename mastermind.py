26a33734776a46ed815f7bb67ce20893cd507e5a
darkcheos <ericdellisse@gmail.com>
2024-03-29 15:49:47 +0100

    1er fin, pour l'instant

--- mastermind.py
+++ mastermind.py
@@ -3,15 +3,28 @@ choixJoueur = []
 
 
 couleurs = ["Blanc", "Bleu", "Rouge", "Vert", "Jaune", "Orange", "Brun"]
+# collection du choix de niveau
 level = ["1: T'es null", "2: Facile", "3: Moyen", "4: Difficile", "5: Connard"]
-
 lvl = 0
 
 print("choisisez votre difficulté")
 print(level)
 choixLevel =  int(input('chiffre2: '))
 
+# nombre d'essai a degrémenter dans la boucle que vous avez crée de votre coder
+match choixLevel:
+    case 1:
+        nombreEssai = -1
+    case 2:
+        nombreEssai = 20
+    case 3:
+        nombreEssai = 15
+    case 4:
+        nombreEssai = 10
+    case 5:
+        nombreEssai = 5
 
+# choix des couleur avec une boucle, pour agmenter le nombre de couleur en fonction des difficulter
 print("Le Maitre de Jeu doit choisir une combinaison de 4 couleurs différentes parmi les suivantes : ")
 print(couleurs)
 
@@ -19,9 +32,3 @@ while lvl < (choixLevel+1):
     
     lvl += 1
     pass
-
-for i in  range(0, 4):
-    print("\nChoisissez la couleur",i+1,":")
-    choixMaitreDeJeu.append(input())
-    
-#def verifCombinaison(choixJoueur):
