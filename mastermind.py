362fe72e78f687b76d0bd33252a53dc807d60a18
Thibault Paillon <thibault_paillon@live.fr>
2024-03-29 14:27:36 +0100

    Correction du for

--- mastermind.py
+++ mastermind.py
@@ -2,10 +2,10 @@ choixMaitreDeJeu = []
 choixJoueur = []
 couleurs = ["Blanc", "Bleu", "Rouge", "Vert", "Jaune", "Orange", "Brun"]
 
-print("Le Maitre de Jeu doit choisir une combinaison de 4 couleurs parmi les suivantes : ")
+print("Le Maitre de Jeu doit choisir une combinaison de 4 couleurs différentes parmi les suivantes : ")
 print(couleurs)
 
-for i in  range(0, len(couleurs)):
+for i in  range(0, 4):
     print("\nChoisissez la couleur",i+1,":")
     choixMaitreDeJeu.append(input())
     
