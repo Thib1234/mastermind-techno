dbeb5b08e2d4112eea97f64b0b35cb5454a54a21
Thibault Paillon <thibault_paillon@live.fr>
2024-03-29 16:03:58 +0100

    Fix merge Eric

--- mastermind.py
+++ mastermind.py
@@ -1,30 +1,10 @@
 choixMaitreDeJeu = [0, 0, 0, 0]
 choixJoueur = []
 playAgain = True
-
-print("Sélectionner un mode de jeu.\n1. Versus CPU\n2. Versus Player")
-modeDeJeu = int(input())
-if modeDeJeu == 1:
-    pass # Mode CPU
-elif modeDeJeu == 2:
-    print("Le Maitre de Jeu doit choisir une combinaison de 4 couleurs différentes parmi les suivantes : ")
-    print(couleurs)
-
-    for i in  range(0, 4):
-        while choixMaitreDeJeu[i] == 0:
-            print("\nChoisissez la couleur",i+1,":")
-            choix = input().capitalize()
-            if choix not in choixMaitreDeJeu and choix in couleurs:
-                choixMaitreDeJeu[i] = choix
-            else:
-                print("Veuillez choisir une couleur unique.")
-    
-#def verifCombinaison(choixJoueur):rouge
-            
-=======
+couleurs = ["Blanc", "Bleu", "Rouge", "Vert", "Jaune", "Orange", "Brun"]
 
 # collection du choix de niveau
-level = ["1: T'es null", "2: Facile", "3: Moyen", "4: Difficile", "5: Connard"]
+level = ["1: T'es nul", "2: Facile", "3: Moyen", "4: Difficile", "5: Connard"]
 lvl = 0
 
 print("choisisez votre difficulté")
@@ -44,12 +24,23 @@ match choixLevel:
     case 5:
         nombreEssai = 5
 
-# choix des couleur avec une boucle, pour agmenter le nombre de couleur en fonction des difficulter
-print("Le Maitre de Jeu doit choisir une combinaison de 4 couleurs différentes parmi les suivantes : ")
-print(couleurs)
+# choix des couleur avec une boucle, pour agmenter le nombre de couleur en fonction des difficulterplayAgain = True
 
-while lvl < (choixLevel+1):
+print("Sélectionner un mode de jeu.\n1. Versus CPU\n2. Versus Player")
+modeDeJeu = int(input())
+if modeDeJeu == 1:
+    pass # Mode CPU
+elif modeDeJeu == 2:
+    print("Le Maitre de Jeu doit choisir une combinaison de 4 couleurs différentes parmi les suivantes : ")
+    print(couleurs)
+
+    for i in  range(0, 4):
+        while choixMaitreDeJeu[i] == 0:
+            print("\nChoisissez la couleur",i+1,":")
+            choix = input().capitalize()
+            if choix not in choixMaitreDeJeu and choix in couleurs:
+                choixMaitreDeJeu[i] = choix
+            else:
+                print("Veuillez choisir une couleur unique.")
     
-    lvl += 1
-    pass
->>>>>>> origin/testcheos
+#def verifCombinaison(choixJoueur):rouge
