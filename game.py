088151d8aa3cfcaa6021a0f8aaa6af6ce799e7a9
thibault vandenbemden <vandenbemden.2110.student@ifosupwavre.be>
2024-03-29 16:21:40 +0100

    commited game.pu

--- game.py
+++ game.py
@@ -1,15 +1,33 @@
 import random
-colors = ["Blanc", "Bleu", "Rouge", "Vert", "Jaune", "Orange", "Brun"]
+couleurs = ["BLANC", "BLEU", "ROUGE", "VERT", "JAUNE", "ORANGE", "BRUN"]
+choixJoueur = [""] * 4  # Initialisez la liste avec des chaînes vides
 
 essais = 0
-def generate_code():
-    return random.sample(colors, 4)
+def generer_couleur():
+    return random.sample(couleurs, 4)
+def gagne():
+    print("C'EST GAGNE")
+    choixJoueur = [""] * 4
+def perdu():
+    print("NUL")
+    choixJoueur = [""] * 4
+
+choixCpu = generer_couleur()
+print(choixCpu)
 
 def get_guess():
-    guess = input(f"Entrez votre supposition {colors}: ")
+    guess = input(f"Entrez votre supposition {couleurs}: ")
     return guess.upper()
 
-print(get_guess())
-
 while essais <= 10:
-    get_guess()
+    for i in range(len(choixCpu)):
+        choixJoueur[i] = get_guess()
+    for j in range(len(choixCpu)):
+        if choixCpu[j] == choixJoueur[j]:
+            print(f"L'élément {j} est indentique et au bon endroit : {choixCpu[j]} et {choixJoueur[j]}")
+        else:
+            print(f"L'élément {j} est différent dans les deux tableaux : {choixCpu[j]} ")
+    essais += 1
+    if choixJoueur == choixCpu:
+        gagne()
+perdu()
