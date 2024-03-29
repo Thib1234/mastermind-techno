9d77d6d447d206b1fe4aa4f53bd3212159c0ddd3
darkcheos <ericdellisse@gmail.com>
2024-03-29 14:41:46 +0100

    collection choix de la difficulter

--- mastermind.py
+++ mastermind.py
@@ -1,6 +1,12 @@
 choixMaitreDeJeu = []
 choixJoueur = []
+choixLevel = []
+
 couleurs = ["Blanc", "Bleu", "Rouge", "Vert", "Jaune", "Orange", "Brun"]
+level = ["T'es null", "Facil", "Moyen", "Difficile", "Connard"]
+
+print("choisisez votre difficulté")
+print(level)
 
 print("Le Maitre de Jeu doit choisir une combinaison de 4 couleurs différentes parmi les suivantes : ")
 print(couleurs)
