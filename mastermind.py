3e133e41e6a3477c7875e10a261f58df8e75aad0
Geurinckx Fabrice <f.geurinckx@stag.technofuturtic.education>
2024-03-29 16:47:30 +0100

    Fusion game + master

--- mastermind.py
+++ mastermind.py
@@ -1,7 +1,21 @@
+import random
 choixMaitreDeJeu = [0, 0, 0, 0]
-choixJoueur = []
+choixJoueur = [""] * 4
 playAgain = True
 couleurs = ["Blanc", "Bleu", "Rouge", "Vert", "Jaune", "Orange", "Brun"]
+essais = 0
+
+def generer_couleur():
+    return random.sample(couleurs, 4)
+def gagne():
+    print("C'EST GAGNE")
+    choixJoueur = [""] * 4
+def perdu():
+    print("NUL")
+    choixJoueur = [""] * 4
+def get_guess():
+    guess = input(f"Entrez votre supposition {couleurs}: ")
+    return guess.upper()
 
 # collection du choix de niveau
 level = ["1: T'es nul", "2: Facile", "3: Moyen", "4: Difficile", "5: Connard"]
@@ -36,7 +50,20 @@ while validationMode:
         print("Veuillez choisir 1 ou 2.")
 
 if modeDeJeu == 1:
-    pass
+    choixCpu = generer_couleur()
+
+    while essais <= 10:
+        for i in range(len(choixCpu)):
+            choixJoueur[i] = get_guess()
+        for j in range(len(choixCpu)):
+            if choixCpu[j] == choixJoueur[j]:
+                print(f"L'élément {j} est indentique et au bon endroit : {choixCpu[j]} et {choixJoueur[j]}")
+            else:
+                print(f"L'élément {j} est différent dans les deux tableaux : {choixCpu[j]} ")
+        essais += 1
+        if choixJoueur == choixCpu:
+            gagne()
+    perdu()
 elif modeDeJeu == 2:
     print("Le Maitre de Jeu doit choisir une combinaison de 4 couleurs différentes parmi les suivantes : ")
     print(couleurs)
