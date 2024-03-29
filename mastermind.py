b68215791b142e8fd51bf3ec6612baca109e493a
darkcheos <ericdellisse@gmail.com>
2024-03-29 14:56:59 +0100

    choix de la difficulter, partie couleur

--- mastermind.py
+++ mastermind.py
@@ -1,15 +1,24 @@
 choixMaitreDeJeu = []
 choixJoueur = []
-choixLevel = []
+
 
 couleurs = ["Blanc", "Bleu", "Rouge", "Vert", "Jaune", "Orange", "Brun"]
-level = ["T'es null", "Facil", "Moyen", "Difficile", "Connard"]
+level = ["T'es null", "Facile", "Moyen", "Difficile", "Connard"]
+
+lvl = 0
 
 print("choisisez votre difficulté")
 print(level)
+choixLevel =  int(input('chiffre2: '))
+
 
 print("Le Maitre de Jeu doit choisir une combinaison de 4 couleurs différentes parmi les suivantes : ")
 print(couleurs)
+
+while lvl < (choixLevel+1):
+    
+    lvl += 1
+    pass
 
 for i in  range(0, 4):
     print("\nChoisissez la couleur",i+1,":")
