34d02893069618fc5becceded5c891b66b878638
thibault vandenbemden <vandenbemden.2110.student@ifosupwavre.be>
2024-03-29 16:48:32 +0100

    update game

--- game.py
+++ game.py
@@ -1,33 +1,43 @@
 import random
-couleurs = ["BLANC", "BLEU", "ROUGE", "VERT", "JAUNE", "ORANGE", "BRUN"]
-choixJoueur = [""] * 4  # Initialisez la liste avec des chaînes vides
 
-essais = 0
+couleurs = ["BLANC", "BLEU", "ROUGE", "VERT", "JAUNE", "ORANGE", "BRUN"]
+
 def generer_couleur():
     return random.sample(couleurs, 4)
+
+def reset_jeu():
+    return [""] * 4, 0
+
 def gagne():
     print("C'EST GAGNE")
-    choixJoueur = [""] * 4
+    return reset_jeu()
+
 def perdu():
     print("NUL")
-    choixJoueur = [""] * 4
-
-choixCpu = generer_couleur()
-print(choixCpu)
+    return reset_jeu()
 
 def get_guess():
     guess = input(f"Entrez votre supposition {couleurs}: ")
     return guess.upper()
 
-while essais <= 10:
-    for i in range(len(choixCpu)):
-        choixJoueur[i] = get_guess()
-    for j in range(len(choixCpu)):
-        if choixCpu[j] == choixJoueur[j]:
-            print(f"L'élément {j} est indentique et au bon endroit : {choixCpu[j]} et {choixJoueur[j]}")
-        else:
-            print(f"L'élément {j} est différent dans les deux tableaux : {choixCpu[j]} ")
-    essais += 1
-    if choixJoueur == choixCpu:
-        gagne()
-perdu()
+def game():
+    choixCpu = generer_couleur()
+    print(choixCpu)
+
+    choixJoueur, essais = reset_jeu()
+
+    while essais <= 10:
+        for i in range(len(choixCpu)):
+            choixJoueur[i] = get_guess()
+        for j in range(len(choixCpu)):
+            if choixCpu[j] == choixJoueur[j]:
+                print(f"L'élément {j} est indentique et au bon endroit : {choixCpu[j]} et {choixJoueur[j]}")
+            else:
+                print(f"L'élément {j} est différent dans les deux tableaux : {choixCpu[j]} ")
+        essais += 1
+        if choixJoueur == choixCpu:
+            choixJoueur, essais = gagne()
+    perdu()
+
+# Pour lancer le jeu, il suffit d'appeler la fonction game
+game()
